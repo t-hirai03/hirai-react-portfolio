c1131a7bc63b91178598184ecbb423d49b2c2560
priv-hirai <hiraitakahiro0923@gmail.com>
2022-09-18 00:31:50 +0900

    ヘッダーのレスポンス対応、mv作成

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import logo from '../assets/image/top/logo.svg';
-import '../assets/scss/top.scss';
+import React from "react";
+import dogIcon from '../assets/image/top/dog_icon.png';
+import scroll_down from '../assets/image/top/scroll_down.svg';
+import "../assets/scss/top.scss";
 
-function App() {
-
+function top() {
   const handleClick = (
     event: React.MouseEvent<HTMLButtonElement | HTMLDivElement, MouseEvent>
   ) => {
@@ -11,24 +11,43 @@ function App() {
   };
 
   return (
-    <div className="App" onClick={handleClick}>
-      {/* <button onClick={handleClick}>test</button> */}
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="top ly_main" onClick={handleClick}>
+      <section className="top-mv">
+        <div className="top-mv_cloud01"></div>
+        <div className="top-mv_cloud02"></div>
+        <div className="top-mv_triangle"></div>
+        <div className="top-mv_bar fadeIn">
+          <h1>
+            ポートフォリオ
+            <br />
+            平井隆裕
+          </h1>
+        </div>
+        <div className="top-mv_dog fadeIn">
+          <img src={dogIcon} alt="dogIcon" />
+        </div>
+        <div className="top-mv_underArea">
+          <a href="#profile" className="top-mv_scrollDown">
+            <img src={scroll_down} className="fadeIn" alt="scroll_down" />
+          </a>
+        </div>
+      </section>
+      {/* <div className="top-header">
+        <img src={logo} className="top-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/top.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="top-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </div>
+      </div> */}
     </div>
   );
 }
 
-export default App;
+export default top;
