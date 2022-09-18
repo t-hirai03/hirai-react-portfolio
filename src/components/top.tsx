fc3fc74751874522e768b8881c3add84ef839178
priv-hirai <hiraitakahiro0923@gmail.com>
2022-09-18 19:56:20 +0900

    chartコンポーネント作成

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import dogIcon from '../assets/image/top/dog_icon.png';
-import scroll_down from '../assets/image/top/scroll_down.svg';
+import dogIcon from "../assets/image/top/dog_icon.png";
+import scrollDownIcon from "../assets/image/top/scroll_down.svg";
+import hiraiFaceImage from "../assets/image/top/face.jpg";
+import gitHubLogo from "../assets/image/top/github_logo.svg";
+import Charts from "../components/chart";
 import "../assets/scss/top.scss";
 
 function top() {
-  const handleClick = (
-    event: React.MouseEvent<HTMLButtonElement | HTMLDivElement, MouseEvent>
-  ) => {
-    console.log("確認");
-  };
+  // const handleClick = (
+  //   event: React.MouseEvent<HTMLButtonElement | HTMLDivElement, MouseEvent>
+  // ) => {
+  //   console.log("確認");
+  // };
 
   return (
-    <div className="top ly_main" onClick={handleClick}>
+    <div className="top ly_main">
       <section className="top-mv">
         <div className="top-mv_cloud01"></div>
         <div className="top-mv_cloud02"></div>
@@ -28,24 +31,115 @@ function top() {
         </div>
         <div className="top-mv_underArea">
           <a href="#profile" className="top-mv_scrollDown">
-            <img src={scroll_down} className="fadeIn" alt="scroll_down" />
+            <img src={scrollDownIcon} className="fadeIn" alt="scroll_down" />
           </a>
         </div>
       </section>
-      {/* <div className="top-header">
-        <img src={logo} className="top-logo" alt="logo" />
-        <p>
-          Edit <code>src/top.tsx</code> and save to reload.
-        </p>
-        <a
-          className="top-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </div> */}
+      <section className="top-section">
+        <div className="ly_inner">
+          <h2 className="top-contents_title">About</h2>
+          <div className="top-about">
+            <div className="top-about_image">
+              <img src={hiraiFaceImage} alt="平井 隆裕" />
+            </div>
+            <div className="top-about_item">
+              <div className="top-about_wrap">
+                <p>平井隆裕 - ひらいたかひろ</p>
+                <p className="top-about_icon">
+                  <a
+                    href="https://github.com/priv-hirai"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src="../assets/image/top/github_logo.svg"
+                      alt="gitHub logo"
+                    />
+                  </a>
+                </p>
+              </div>
+              <dl className="top-about_dl">
+                <dt>生年月日：</dt>
+                <dd>1995年9月23日</dd>
+              </dl>
+              <dl className="top-about_dl">
+                <dt>出身地：</dt>
+                <dd>福井県福井市</dd>
+              </dl>
+              <dl className="top-about_dl">
+                <dt>趣味：</dt>
+                <dd>
+                  グルメ巡り
+                  <br />
+                  スポーツ観戦（バスケ、テニス）
+                  <br />
+                  ゲーム
+                </dd>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="top-section">
+        <div className="ly_inner">
+          <h2 className="top-contents_title">My skill set</h2>
+          <div>
+            <p className="top-skill_text">
+              修練のすえ身につけたスキルをグラフィカルにまとめました。広く浅くではありますがフロントからバックエンドまで幅広くスキルアップに取り組んできました。
+              オールラウンドに対応できる点が強みですが、全体的に理解が浅く、専門性の低さを認識しています。今後はバックエンド系の開発（クラウド含む）の業務に携わる比率を増やし、
+              少しずつ専門性を高めていきたいと考えています。
+            </p>
+            <p>＊業務で実際に使用した技術のみ掲載しております。</p>
+          </div>
+          <div className="top-chart">
+            <div className="top-chart_contents">
+              <Charts
+                titleLabel={"font-end"}
+                Label01={"HTML/CSS"}
+                data01={3}
+                Label02={"JavaScript"}
+                data02={3}
+                Label03={"jQuery"}
+                data03={3}
+                Label04={"Nuxt.js/Vue.js"}
+                data04={3}
+                Label05={"Wordpress"}
+                data05={2}
+                Label06={""}
+                data06={0}
+                graphBackgroundColor={"rgba(255, 99, 132, 0.2)"}
+                graphBorderColor={"rgb(255, 99, 132)"}
+              />
+            </div>
+            <div className="top-chart_contents">
+              <Charts
+                titleLabel={"back-end"}
+                Label01={"Java"}
+                data01={3}
+                Label02={"PostgreSQL"}
+                data02={3}
+                Label03={"Oracle"}
+                data03={1}
+                Label04={""}
+                data04={0}
+                Label05={""}
+                data05={0}
+                Label06={""}
+                data06={0}
+                graphBackgroundColor={"rgba(75, 192, 192, 0.2)"}
+                graphBorderColor={"rgb(75, 192, 192)"}
+              />
+            </div>
+          </div>
+          <div>
+            <p className="top-skill_text">
+              チャートの見方としては「1: 軽く使用した程度」、「2:
+              実務で数ヶ月以上使用しているがもう少し習熟が必要」、「3:
+              実務レベルで自由に駆使できる」。という感じです。
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
