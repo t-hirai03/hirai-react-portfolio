c3ef3e3506aed351f8dd31ff6f547a584bf3969c
DESKTOP-NT9H6LD\hirai <hirai@j-cool.fm>
2022-09-20 12:23:35 +0900

    GSAPのプラグインscrollTriggerを導入

--- src/components/gsap.tsx
+++ src/components/gsap.tsx
@@ -1,13 +1,20 @@
 import "../style.css";
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function App() {
+    gsap.registerPlugin(ScrollTrigger);
   const div = useRef<HTMLDivElement>(null);
   useEffect(() => {
     gsap.to(div.current, {
-      backgroundImage: "linear-gradient(to left, #30CFD0, #330867)",
-      duration: 5
+    //   backgroundImage: "linear-gradient(to left, #30CFD0, #330867)",
+    //   duration: 5
+    scrollTrigger: div.current,
+    x: 500, // x軸方向に500px移動する
+    rotation: 180, // 180度回転する
+    duration: 3, // 3秒間アニメーションする
+    // delay: 3, // 3秒後にアニメーションする
     });
   }, [div]);
   return (
