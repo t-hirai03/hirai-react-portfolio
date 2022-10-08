1140f658c70c052e889b7726bc401b6d993d6fa0
priv-hirai <hiraitakahiro0923@gmail.com>
2022-10-08 14:46:34 +0900

    不要な処理、ファイルを削除

--- src/components/gitHubIcon.tsx
+++ src/components/gitHubIcon.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, VFC } from 'react';
+import React, { useEffect } from 'react';
 import { css, keyframes } from '@emotion/css';
 import "../assets/scss/components/gitHubIcon.scss";
 
