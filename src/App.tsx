2fe19e61ae5fa3b4c2406803640a15d35a0cee5a
priv-hirai <hiraitakahiro0923@gmail.com>
2023-03-04 16:34:09 +0900

    初期表示のローディング表示を削除

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import HeaderSection from "./components/header";
 import Top from "./page/top";
 import FooterSection from "./components/footer";
-import AudioLoading from "./components/audioLoading";
 import RevolvingDotLoading from "./components/revolvingDotLoading";
 import { useSelector } from 'react-redux'
 import { selectCount } from './store/reducer'
@@ -22,14 +21,9 @@ const App = () => {
       {/* ヘッダー */}
       <HeaderSection />
       {/* コンテンツ */}
-      {isLoading ? (
-        <AudioLoading />
-      ) : (
-        <div>
-          <Top />
-          <FooterSection />
-        </div>
-      )}
+      <Top />
+      {/* フッター */}
+      <FooterSection />
     </div>
   );
 }
