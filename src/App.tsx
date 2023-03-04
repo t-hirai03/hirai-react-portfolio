618e3155564ab0dfdc1745a28ed49bafd3e512df
priv-hirai <hiraitakahiro0923@gmail.com>
2023-03-05 00:43:00 +0900

    天気予報のAPIから天気情報取得処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,6 @@ import { useSelector } from 'react-redux'
 import { selectCount } from './store/reducer'
 
 const App = () => {
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  setTimeout(() => {
-    setIsLoading(false);
-  }, 2000);
-
   const isRevolvingDotLoading = useSelector(selectCount);
 
   return (
